f5f0c04f6838b837c9d180f71c428849f2e78d32
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2023-12-21 18:19:05 +0100

    rimossi dati di mubench superiori a 30 utenti aggiunta metrica di distanza di hamming nel calcolo delle metriche in questions_utils.py

--- models_comparison/questions_utils.py
+++ models_comparison/questions_utils.py
@@ -3,6 +3,7 @@ import os
 import lingam
 import networkx as nx
 import numpy as np
+import pandas as pd
 import torch
 from dagma.linear import DagmaLinear
 from dagma.nonlinear import DagmaMLP, DagmaNonlinear
@@ -15,7 +16,10 @@ loads = ['uniform', 'randomly_balanced', 'unbalanced_one']
 SRs = [1, 5, 10]
 treatments = ['NUSER', 'LOAD_0', 'LOAD_1', 'LOAD_2', 'SR']
 all_metrics = ['RES_TIME', 'CPU', 'MEM']
-path_wm = "../mubench/configs/workmodel.json"
+mubench_path = "../mubench"
+path_df = mubench_path + "/data/mubench_df.csv"
+path_wm = mubench_path + "/configs/workmodel.json"
+path_minimal_configs_anomaly = mubench_path + "/minimal_configs_anomaly.json"
 model_limit = 30
 
 
@@ -85,63 +89,110 @@ def gen_configs_per_metric(df_discovery, loads_mapping, path_cgraph, path_config
                                      stability=0, nuser_limit=model_limit)
 
 
-def check_anomalies(df, ser, metrics=None):
+def create_min_anomalies_file(df, path, metrics=None):
     if metrics is None:
         metrics = all_metrics
 
     ns = list(set(df['NUSER']))
-    ns.sort(reverse=True)
 
-    for n in ns:
-        for l in loads:
-            for sr in SRs:
-                ths = utils.calc_thresholds(df, ser)
-                for met in metrics:
-                    if df[(df['NUSER'] == n) & (df['LOAD'] == l) & (df['SR'] == sr)][met + '_' + ser].mean() > ths[met]:
-                        return True
-    return False
+    out_j = {}
+    for ser in services:
+        out_j[ser] = {}
+        ths = utils.calc_thresholds(df, ser)
+        for met in metrics:
+            for n in ns:
+                icomb = []
+                for l in loads:
+                    for sr in SRs:
+                        if df[(df['NUSER'] == n) & (df['LOAD'] == l) & (df['SR'] == sr)][met + '_' + ser].mean() > ths[
+                            met]:
+                            icomb.append({'NUSER': n, 'LOAD': l, 'SR': sr})
+
+                # out_j[ser][met] = {"NUSER": n, 'LOAD': l, 'SR': sr}
+                if len(icomb) > 0:
+                    out_j[ser][met] = icomb
+                    break
+
+    with open(path, 'w') as f_out:
+        json.dump(out_j, f_out)
 
 
 def calc_metrics(df, path_configs, metrics=None):
     if metrics is None:
         metrics = all_metrics
 
-    true_positive = 0
-    false_negative = 0
-    false_positive = 0
+    def calc_hamming_distance(conf_real, conf_pred):
+        # a = abs(conf_real['NUSER'] - conf_pred['nusers'][0])
+        # b = 0 if conf_real['LOAD'] == conf_pred['loads'][0] else 1
+        # c = 0 if conf_real['SR'] == conf_pred['spawn_rates'][0] else 1
+        # d = (conf_pred['spawn_rates'][0])
+        # print("a:{} b:{} c:{} d:{}".format(a, b, c, d))
 
-    for ser in services:
-        path_config = path_configs + "_" + ser + ".json"
-        ths = utils.calc_thresholds(df, ser)
+        return abs(conf_real['NUSER'] - conf_pred['nusers'][0]) + (
+            0 if conf_real['LOAD'] == conf_pred['loads'][0] else 1) + (
+            0 if conf_real['SR'] == conf_pred['spawn_rates'][0] else 1)
 
-        if os.path.exists(path_config):
-            with open(path_config, 'r') as f_c:
-                config = json.load(f_c)
+    if not os.path.exists(path_minimal_configs_anomaly):
+        create_min_anomalies_file(pd.read_csv(path_df), path_minimal_configs_anomaly)
 
-                df_config = df[(df['NUSER'] == config['nusers'][0]) &
-                               (df['LOAD'] == config['loads'][0]) &
-                               (df['SR'] == config['spawn_rates'][0])]
+    with open(path_minimal_configs_anomaly, 'r') as f_minimal_conf:
+        minimal_confs = json.load(f_minimal_conf)
 
-                # X - Positive
-                for met in config['anomalous_metrics']:
-                    if df_config[met + "_" + ser].mean() > ths[met]:  # True - Positive
-                        true_positive += 1
-                    else:  # False - Positive
-                        false_positive += 1
-        else:  # X - Negative
-            # check there is anomalies for service ser
-            if check_anomalies(df, ser, metrics=metrics):  # False - Negative
-                false_negative += 1
+        true_positive = 0
+        false_negative = 0
+        false_positive = 0
+        dists = []
 
-    precision = 0
-    recall = 0
+        for ser in services:
 
-    if true_positive > 0:
-        precision = true_positive / (true_positive + false_positive)
+            path_config = path_configs + "_" + ser + ".json"
+            ths = utils.calc_thresholds(df, ser)
 
-        if false_negative > 0:
-            recall = true_positive / (true_positive + false_negative)
-        else:
-            recall = 1
+            if os.path.exists(path_config):
+                with open(path_config, 'r') as f_c:
+                    config = json.load(f_c)
 
-    return {'precision': precision, 'recall': recall}
+                    df_config = df[(df['NUSER'] == config['nusers'][0]) &
+                                   (df['LOAD'] == config['loads'][0]) &
+                                   (df['SR'] == config['spawn_rates'][0])]
+
+                    # X - Positive
+                    for met in config['anomalous_metrics']:
+                        if df_config[met + "_" + ser].mean() > ths[met]:  # True - Positive
+                            true_positive += 1
+                            dists_c = np.zeros(len(minimal_confs[ser][met]), dtype=int)
+                            for i, mc in enumerate(minimal_confs[ser][met]):
+                                dists_c[i] = calc_hamming_distance(mc, config)
+                                # print("R: ({},{},{}) P: ({},{},{}) -> {}".format(mc['NUSER'], mc['LOAD'], mc['SR'],
+                                #                                                 config['nusers'][0],
+                                #                                                 config['loads'][0],
+                                #                                                 config['spawn_rates'][0],
+                                #                                                 calc_hamming_distance(mc, config)))
+
+                            dists.append(min(dists_c))
+                        else:  # False - Positive
+                            false_positive += 1
+
+            else:  # X - Negative
+                false_negative = 0
+                # check there is anomalies for service ser
+                # if check_anomalies(df, ser, metrics=metrics):  # False - Negative
+                if len(minimal_confs[ser]) > 0:  # False - Negative
+                    false_negative += 1
+
+        precision = 0
+        recall = 0
+
+        if true_positive > 0:
+            precision = true_positive / (true_positive + false_positive)
+
+            if false_negative > 0:
+                recall = true_positive / (true_positive + false_negative)
+            else:
+                recall = 1
+
+        return {'precision': precision, 'recall': recall, 'mean_hamming_distance': np.mean(dists)}
+
+
+m = calc_metrics(pd.read_csv(path_df), "question1/configs/dlingam_prior_RES_TIME", metrics=['RES_TIME'])
+print(m)
