a3a2abf0697e5d549a532bba673b74218ea17f2b
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2023-12-28 16:44:02 +0100

    compattato con stability e aggiunti alcuni parametri

--- configuration_generator.py
+++ configuration_generator.py
@@ -1,76 +1,35 @@
 import json
+import random
 import dowhy.gcm as gcm
 import utils
 
 
-# funziona solo se il modello non presenta archi entrati nei trattamenti,
-# in questo modo si mantengono i loro range. per esempio i load rimangono binari
-def get_config_from_sample(samples, loads_mapping):
-    # NUSER
-    nuser = int(samples['NUSER'].mean())
-
-    # LOAD
-    mcount = 0
-    icount = None
-    name_l = ""
-    for k, v in loads_mapping.items():
-        count = len(samples[samples['LOAD_' + str(v.index(1))] == 1].values)
-        if count > mcount:
-            mcount = count
-            icount = v
-            name_l = k
-    #  SR
-    sr = int(samples[samples['LOAD_' + str(icount.index(1))] == 1]['SR'].mode().values[0])
-    return nuser, name_l, sr
-
-
-def generate_config(causal_model, df, service, path_config, loads_mapping, metrics=None,
-                    stability=0, nuser_limit=-1):
+def generate_config(causal_model, df, service, path_config, loads_mapping, nuser_limit, nuser_start=2, n_step=1,
+                    metrics=None, stability=0):
     def search_config():
-        for load_level in load_levels:
-            for sr_level in sr_levels:
-
+        random.shuffle(combinations)
+        for load_level, sr_level in combinations:
+            anomalous_metrics = metrics.copy()
+            for stab in range(stability + 1):
+                # print("CHECK: N{}".format(stab))
                 samples = gcm.interventional_samples(causal_model,
-                                                     {'NUSER': lambda y: n,
+                                                     {'NUSER': lambda y: n + stab,
                                                       'LOAD_0': lambda y: loads_mapping[load_level][0],
                                                       'LOAD_1': lambda y: loads_mapping[load_level][1],
                                                       'LOAD_2': lambda y: loads_mapping[load_level][2],
                                                       'SR': lambda y: sr_level},
-                                                     num_samples_to_draw=10000)
+                                                     num_samples_to_draw=1000)
 
-                for met in metrics:
-                    if samples[met + "_" + service].mean() > ths[met]:
-                        anomalous_metrics.append(met)
-                # TODO: compattare con stability
-                if len(anomalous_metrics) > 0:
-                    # print("THRESHOLD EXCEEDED for: " + str(anomalous_metrics))
-
-                    anomalous_metrics_after_check = anomalous_metrics
-                    for stab in range(1, stability + 1):
-                        # print("TEST STABILITY " + str(stab))
-                        samples_stab = gcm.interventional_samples(causal_model, {'NUSER': lambda y: n + stab},
-                                                                  num_samples_to_draw=10000)
-                        for met_anom in anomalous_metrics:
-                            if samples_stab[met_anom + "_" + service].mean() < ths[met_anom]:
-                                anomalous_metrics_after_check.remove(met_anom)
-                                if len(anomalous_metrics_after_check) == 0:
-                                    break
-
-                    if len(anomalous_metrics_after_check) > 0:
-                        # print("THRESHOLD EXCEEDED AFTER STABILITY CHECK for: " + str(anomalous_metrics_after_check))
-                        samples_filterd = samples
-                        for anom_met in anomalous_metrics_after_check:  # TODO: controllare che il samples filtrato non risulti vuoto
-                            samples_filterd = samples_filterd[samples_filterd[anom_met + "_" + service] > ths[anom_met]]
-
-                        nuser, load, sr = get_config_from_sample(samples_filterd, loads_mapping)
-
-                        with open(path_config, 'w') as f_out:
-                            json.dump(
-                                {"nusers": [nuser], "loads": [load], "spawn_rates": [sr],
-                                 "anomalous_metrics": anomalous_metrics_after_check},
-                                f_out)
-                        return True
-        return False
+                for amet in anomalous_metrics.copy():
+                    if samples[amet + "_" + service].mean() <= ths[amet]:
+                        anomalous_metrics.remove(amet)
+                if len(anomalous_metrics) == 0:
+                    return False
+            # len(anomalous_metrics) > 0
+            with open(path_config, 'w') as f_out:
+                json.dump({"nusers": [n], "loads": [load_level], "spawn_rates": [sr_level],
+                           "anomalous_metrics": anomalous_metrics}, f_out)
+                return True
 
     if metrics is None:
         metrics = ['RES_TIME', 'CPU', 'MEM']
@@ -78,9 +37,8 @@ def generate_config(causal_model, df, service, path_config, loads_mapping, metri
 
     load_levels = loads_mapping.keys()
     sr_levels = list(set(df['SR']))
-    n = 2
-    n_step = 1
-    anomalous_metrics = []
-    while not search_config() and (nuser_limit != -1 and n < nuser_limit):
-        print("NUSER: " + str(n))
+    combinations = [(load, sr) for load in load_levels for sr in sr_levels]
+    n = nuser_start
+    while not search_config() and n < nuser_limit:
+        # print("NUSER: " + str(n))
         n += n_step
