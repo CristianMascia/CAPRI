a3f4ad6b5ab27a2f80df2c38086d8f7facf721ef
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2023-12-29 03:01:37 +0100

    aggiunta possibilita di realizzare piu repliche per le questions

--- models_comparison/all_questions.py
+++ models_comparison/all_questions.py
@@ -6,14 +6,20 @@ import importlib
 
 
 def __main__():
+    n_reps = 3
+    n_questions = 4
+
     path_df = "../mubench/data/mubench_df.csv"
     if not os.path.exists(path_df):
         data_preparation.read_experiments("../mubench/data/", {s: s for s in CONFIG.services}).to_csv(path_df,
                                                                                                       index=False)
-    for i in range(4):
-        q = "question{}".format(i)
-        importlib.import_module(q)
-        getattr(sys.modules[q], "__main__")(path_df, q)
+
+    for rep in range(n_reps):
+        print("REPETITION {}".format(rep))
+        for i in range(n_questions):
+            q = "question{}".format(i)
+            importlib.import_module(q)
+            getattr(sys.modules[q], "__main__")(path_df, "{}_REP_{}".format(q, rep))
 
 
-__main__()
+
