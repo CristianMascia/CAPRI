690fc7496e26f499e54e0ab680993d2e17f966de
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2024-01-11 15:54:31 +0100

    risolto BUG

--- configuration_generator.py
+++ configuration_generator.py
@@ -41,14 +41,65 @@ def generate_config(causal_model, df, service, path_config, loads_mapping, nuser
         metrics = ['RES_TIME', 'CPU', 'MEM']
 
     if loads is None:
-        load_levels = loads_mapping.keys()
+        loads = loads_mapping.keys()
 
     if spawn_rates is None:
-        sr_levels = list(set(df['SR']))
+        spawn_rates = list(set(df['SR']))
 
     ths = utils.calc_thresholds(df, service)
 
-    combinations = [(load, sr) for load in load_levels for sr in sr_levels]
+    combinations = [(load, sr) for load in loads for sr in spawn_rates]
+    n = nuser_start
+
+    while not search_config() and n < nuser_limit:
+        n += n_step
+
+
+def generate_config_fast(causal_model, df, service, path_config, loads_mapping, nuser_limit, nuser_start=2, n_step=1,
+                         metrics=None, stability=0, loads=None, spawn_rates=None, show_comment=False):
+    def identity_lambda(number):
+        return lambda x: number
+
+    def search_config():
+        random.shuffle(combinations)
+        for load_level, sr_level in combinations:
+            anomalous_metrics = metrics.copy()
+            for stab in range(stability + 1):
+                # print("CHECK N{}".format(stab))
+
+                dict_int = {'NUSER': lambda y: n + stab, 'SR': lambda y: sr_level}
+                for li in range(len(loads_mapping.keys())):
+                    dict_int['LOAD_{}'.format(li)] = identity_lambda(loads_mapping[load_level][li])
+
+                samples = gcm.interventional_samples(causal_model, dict_int, num_samples_to_draw=1000)
+
+                for amet in anomalous_metrics.copy():
+                    if show_comment:
+                        print("for {} Pred {} TH {} with ({},{},{})".format(amet, samples[amet + "_" + service].mean(),
+                                                                            ths[amet], n, load_level, sr_level))
+                    if samples[amet + "_" + service].mean() <= ths[amet]:
+                        anomalous_metrics.remove(amet)
+                if len(anomalous_metrics) == 0:
+                    break
+            if len(anomalous_metrics) > 0:
+                with open(path_config, 'w') as f_out:
+                    json.dump({"nusers": [n], "loads": [load_level], "spawn_rates": [sr_level],
+                               "anomalous_metrics": anomalous_metrics}, f_out)
+                    return True
+        return False
+
+    if metrics is None:
+        metrics = ['RES_TIME', 'CPU', 'MEM']
+
+    if loads is None:
+        loads = loads_mapping.keys()
+
+    if spawn_rates is None:
+        spawn_rates = list(set(df['SR']))
+
+    ths = utils.calc_thresholds(df, service)
+
+    combinations = [(load, sr) for load in loads for sr in spawn_rates]
     n = nuser_start
 
     while not search_config() and n < nuser_limit:
