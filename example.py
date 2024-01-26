75e063bcec24b560ecabe140ac35ab7ebd143e3f
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2024-01-26 18:51:46 +0100

    ristrutturato example.py

--- example.py
+++ example.py
@@ -1,34 +1,95 @@
 import json
 import os.path
-
 import pandas as pd
-
 import data_preparation
 import data_visualization
 import utils
 from causal_model_generator import build_model
 from configuration_generator import generate_config
+from enum import Enum
+from performance_evaluator import calc_metrics
 
 
-def system_workflow(path_exp, path, architecture, pods, mapping, model_limit, nuser_start=2, ths_filtered=False,
-                    generation_conf_FAST=False):
-    path_df = os.path.join(path, "df.csv")
-    path_df_disc = os.path.join(path, "df_discovery.csv")
-    path_thresholds = os.path.join(path, "thresholds.json")
-    path_prior = os.path.join(path, "prior_knowledge")
-    path_dag = os.path.join(path, "dag")
-    path_configs = os.path.join(path, "generated_configs")
+class System(Enum):
+    MUBENCH = "mubench"
+    SOCKSHOP = "sockshop"
+    TRAINTICKET = "trainticket"
 
-    if not os.path.exists(path):
-        os.mkdir(path)
+
+def get_system_info(system):
+    if system == System.MUBENCH:
+        services = ['s' + str(i) for i in range(10)]
+        mapping = {s: s for s in services}
+        pods = services
+        arch = utils.get_architecture_from_wm("mubench/configs/workmodel.json")
+    else:
+        with open("sockshop/architecture.json", 'r') as f_arch:
+            with open(os.path.join(str(system.value), "mapping_service_request.json"), 'r') as f_map:
+                with open(os.path.join(str(system.value), "mapping_service_request.json"), 'r') as f_pods:
+                    arch = json.load(f_arch)
+                    mapping = json.load(f_map)
+                    services = list(set(mapping))
+                    pods = [p.replace("\n", "") for p in f_pods.readlines()]
+
+    return services, mapping, pods, arch
+
+
+def create_dataset(system, path_df=None, path_exp=None):
+    services, mapping, pods, arch = get_system_info(system)
+    if path_df is None:
+        path_df = os.path.join(str(system.value), "data", str(system.value) + "_df.csv")
+    if path_exp is None:
+        path_exp = os.path.join(str(system.value), "data")
+    df = data_preparation.read_experiments(path_exp, mapping, pods)
+    df.to_csv(path_df, index=False)
+    return df
+
+
+def system_visualization(system, path_png=None):
+    if path_png is None:
+        path_png = os.path.join(str(system.value), "png")
+
+    services, _, _, _ = get_system_info(system)
+    path_df = os.path.join(str(system.value), "data", str(system.value) + "_df.csv")
+
+    if not os.path.exists(path_df):
+        df = create_dataset(system)
+    else:
+        df = pd.read_csv(path_df)
+
+    if system == System.MUBENCH:
+        df_disc = df[df['NUSER'].isin([i for i in range(1, 30, 2)] + [30])]
+        data_visualization.___main__(df_disc, path_png, services)
+    else:
+        data_visualization.___main__(df, path_png, services, True)
+
+
+def system_workflow(system, path_work, generation_conf_FAST=False):
+    services, mapping, pods, architecture = get_system_info(system)
+
+    path_df = os.path.join(path_work, "df.csv")
+    path_df_disc = os.path.join(path_work, "df_discovery.csv")
+    path_thresholds = os.path.join(path_work, "thresholds.json")
+    path_prior = os.path.join(path_work, "prior_knowledge")
+    path_dag = os.path.join(path_work, "dag")
+    path_configs = os.path.join(path_work, "generated_configs")
+
+    if system == System.MUBENCH:
+        ths_filtered = False
+        model_limit = 30
+        nuser_start = 2
+    else:
+        ths_filtered = True
+        model_limit = 50
+        nuser_start = 4
+
+    if not os.path.exists(path_work):
+        os.mkdir(path_work)
     if not os.path.exists(path_configs):
         os.mkdir(path_configs)
 
-    services = list(mapping.keys())
-
     print("------READING EXPERIMENTS------")
-    df = data_preparation.read_experiments(path_exp, mapping, pods, data_preparation.rename_startwith)
-    df.to_csv(path_df, index=False)
+    df = create_dataset(system, path_df=path_df, path_exp=os.path.join(str(system.value), "data"))
     df_discovery, loads_mapping = utils.hot_encode_col_mapping(df, 'LOAD')
     df_discovery.to_csv(path_df_disc, index=False)
 
@@ -56,74 +117,14 @@ def system_workflow(path_exp, path, architecture, pods, mapping, model_limit, nu
                             FAST=generation_conf_FAST, ths_filtered=ths_filtered, nuser_start=nuser_start)
 
 
-def muBench_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
-    path_system = "mubench"
-    path_exp = os.path.join(path_system, "data")
-    path_wm = os.path.join(path_system, "configs", "workmodel.json")
-    services = ['s' + str(i) for i in range(10)]
-    system_workflow(path_exp, path_work, utils.get_architecture_from_wm(path_wm), services, {s: s for s in services},
-                    model_limit=30, nuser_start=2, ths_filtered=ths_filtered,
-                    generation_conf_FAST=generation_conf_FAST)
+def system_performance_evaluation(system, path_work):
+    path_df = os.path.join(path_work, "df.csv")
+    path_metrics = os.path.join(path_work, "metrics.json")
+    path_configs = os.path.join(path_work, "generated_configs")
+    path_run_configs = os.path.join(path_work, "run_configs")
 
+    services, mapping, _, _ = get_system_info(system)
+    metrics = calc_metrics(path_df, path_configs, path_run_configs, services, mapping)
 
-def sockshop_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
-    path_system = "sockshop"
-    path_exp = os.path.join(path_system, "data")
-    path_arch = os.path.join(path_system, "architecture.json")
-    path_pods = os.path.join(path_system, "pods.txt")
-    path_mapping = os.path.join(path_system, "mapping_service_request.json")
-
-    with open(path_arch, 'r') as f_arch:
-        with open(path_pods, 'r') as f_pods:
-            pods = [p.replace("\n", "") for p in f_pods.readlines()]
-            with open(path_mapping, "r") as f_map:
-                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map), model_limit=50,
-                                nuser_start=4, ths_filtered=ths_filtered,
-                                generation_conf_FAST=generation_conf_FAST)
-
-
-def trainticket_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
-    path_system = "trainticket"
-    path_exp = os.path.join(path_system, "data")
-    path_arch = os.path.join(path_system, "architecture.json")
-    path_pods = os.path.join(path_system, "pods.txt")
-    path_mapping = os.path.join(path_system, "mapping_service_request.json")
-    with open(path_arch, 'r') as f_arch:
-        with open(path_pods, 'r') as f_pods:
-            pods = [p.replace("\n", "") for p in f_pods.readlines()]
-            with open(path_mapping, "r") as f_map:
-                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map), model_limit=30,
-                                nuser_start=4, ths_filtered=ths_filtered,
-                                generation_conf_FAST=generation_conf_FAST)
-
-
-def mubench_visualization():
-    path_system = "mubench"
-    path_df = os.path.join(path_system, "data", "mubench_df.csv")
-    path_png = os.path.join(path_system, "png")
-
-    df = pd.read_csv(path_df)
-    df_disc = df[df['NUSER'].isin([i for i in range(1, 30, 2)] + [30])]
-    data_visualization.___main__(df_disc, path_png, ['s' + str(i) for i in range(10)])
-
-
-def sockshop_visualization():
-    path_system = "sockshop"
-    path_mapping = os.path.join(path_system, "mapping_service_request.json")
-    path_data = os.path.join(path_system, "data")
-    path_df = os.path.join(path_data, "sockshop_df.csv")
-    path_png = os.path.join(path_system, "png")
-
-    with open(path_mapping, 'r') as f_map:
-        data_visualization.___main__(pd.read_csv(path_df), path_png, list(set(json.load(f_map).keys())), True)
-
-
-def trainticket_visualization():
-    path_system = "trainticket"
-    path_mapping = os.path.join(path_system, "mapping_service_request.json")
-    path_data = os.path.join(path_system, "data")
-    path_df = os.path.join(path_data, "trainticket_df.csv")
-    path_png = os.path.join(path_system, "png")
-
-    with open(path_mapping, 'r') as f_map:
-        data_visualization.___main__(pd.read_csv(path_df), path_png, list(set(json.load(f_map).keys())))
+    with open(path_metrics, 'w') as f_metrics:
+        json.dump(metrics, f_metrics)
