607c8fb1da03cbfb5dd2049e44f372283833e9cc
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2024-01-26 16:52:12 +0100

    aggiunta possibilita di specificare un path per la genrazione aggiunta possibilita di settare nuser start e model limit

--- example.py
+++ example.py
@@ -10,7 +10,8 @@ from causal_model_generator import build_model
 from configuration_generator import generate_config
 
 
-def system_workflow(path_exp, path, architecture, pods, mapping, ths_filtered=False, generation_conf_FAST=False):
+def system_workflow(path_exp, path, architecture, pods, mapping, model_limit, nuser_start=2, ths_filtered=False,
+                    generation_conf_FAST=False):
     path_df = os.path.join(path, "df.csv")
     path_df_disc = os.path.join(path, "df_discovery.csv")
     path_thresholds = os.path.join(path, "thresholds.json")
@@ -51,24 +52,23 @@ def system_workflow(path_exp, path, architecture, pods, mapping, ths_filtered=Fa
             print("Searching configuration for service: {} for metric: {}".format(ser, met))
             generate_config(causal_model, df_discovery, ser,
                             os.path.join(path_configs, "{}_{}_{}.json".format("configs", met, ser)), loads_mapping,
-                            metrics=[met], stability=0, nuser_limit=50, show_comment=True, FAST=generation_conf_FAST,
-                            ths_filtered=ths_filtered)
+                            metrics=[met], stability=0, nuser_limit=model_limit, show_comment=True,
+                            FAST=generation_conf_FAST, ths_filtered=ths_filtered, nuser_start=nuser_start)
 
 
-def muBench_workflow(ths_filtered=False, generation_conf_FAST=False):
+def muBench_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
     path_system = "mubench"
     path_exp = os.path.join(path_system, "data")
-    path_work = os.path.join(path_system, "work")
     path_wm = os.path.join(path_system, "configs", "workmodel.json")
     services = ['s' + str(i) for i in range(10)]
     system_workflow(path_exp, path_work, utils.get_architecture_from_wm(path_wm), services, {s: s for s in services},
-                    ths_filtered=ths_filtered, generation_conf_FAST=generation_conf_FAST)
+                    model_limit=30, nuser_start=2, ths_filtered=ths_filtered,
+                    generation_conf_FAST=generation_conf_FAST)
 
 
-def sockshop_workflow(ths_filtered=False, generation_conf_FAST=False):
+def sockshop_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
     path_system = "sockshop"
     path_exp = os.path.join(path_system, "data")
-    path_work = os.path.join(path_system, "work")
     path_arch = os.path.join(path_system, "architecture.json")
     path_pods = os.path.join(path_system, "pods.txt")
     path_mapping = os.path.join(path_system, "mapping_service_request.json")
@@ -77,14 +77,14 @@ def sockshop_workflow(ths_filtered=False, generation_conf_FAST=False):
         with open(path_pods, 'r') as f_pods:
             pods = [p.replace("\n", "") for p in f_pods.readlines()]
             with open(path_mapping, "r") as f_map:
-                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map),
-                                ths_filtered=ths_filtered, generation_conf_FAST=generation_conf_FAST)
+                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map), model_limit=50,
+                                nuser_start=4, ths_filtered=ths_filtered,
+                                generation_conf_FAST=generation_conf_FAST)
 
 
-def trainticket_workflow(ths_filtered=False, generation_conf_FAST=False):
+def trainticket_workflow(path_work, ths_filtered=False, generation_conf_FAST=False):
     path_system = "trainticket"
     path_exp = os.path.join(path_system, "data")
-    path_work = os.path.join(path_system, "work")
     path_arch = os.path.join(path_system, "architecture.json")
     path_pods = os.path.join(path_system, "pods.txt")
     path_mapping = os.path.join(path_system, "mapping_service_request.json")
@@ -92,8 +92,9 @@ def trainticket_workflow(ths_filtered=False, generation_conf_FAST=False):
         with open(path_pods, 'r') as f_pods:
             pods = [p.replace("\n", "") for p in f_pods.readlines()]
             with open(path_mapping, "r") as f_map:
-                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map),
-                                ths_filtered=ths_filtered, generation_conf_FAST=generation_conf_FAST)
+                system_workflow(path_exp, path_work, json.load(f_arch), pods, json.load(f_map), model_limit=30,
+                                nuser_start=4, ths_filtered=ths_filtered,
+                                generation_conf_FAST=generation_conf_FAST)
 
 
 def mubench_visualization():
@@ -126,4 +127,3 @@ def trainticket_visualization():
 
     with open(path_mapping, 'r') as f_map:
         data_visualization.___main__(pd.read_csv(path_df), path_png, list(set(json.load(f_map).keys())))
-
